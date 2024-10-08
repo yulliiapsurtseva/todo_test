a3420a0ea9103b805eb5bdefd65c9269666460c1
yulliiapsurtseva <yulyapsurtseva@gmail.com>
2024-10-08 11:12:02 +0200

    Added faker

--- tests/signDocument.js
+++ tests/signDocument.js
@@ -1,6 +1,7 @@
 import signDocumentPage from '../pages/SignDocumentPage';
 import signDocumentPageData from '../pageData/SignDocumentPage';
 import * as config from '../testData/global';
+import { faker } from '@faker-js/faker';
 
 fixture`Sign the Document`
   .page(config.signDocURL);
@@ -8,7 +9,7 @@ fixture`Sign the Document`
 test('Sign the document', async t => {
     await t
       .click(signDocumentPage.fullnameField)
-      .typeText(signDocumentPage.fullnameField, 'Test Name')
+      .typeText(signDocumentPage.fullnameField, faker.person.fullName())
       .click(signDocumentPage.nextButton)
       .expect(signDocumentPage.confirmationModal.exists).ok('Confirmation modal is absent');
     await t.takeElementScreenshot(signDocumentPage.confirmationModal);
